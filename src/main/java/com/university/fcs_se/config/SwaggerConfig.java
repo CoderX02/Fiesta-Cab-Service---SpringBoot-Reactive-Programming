31ce679446343676c429421ab6845c0e9a7bccc0
Rithika Hettiararchchi <rithika.h@hsenidoutsourcing.com>
2022-09-08 06:34:30 +0530

    Added swagger config file

---
+++ src/main/java/com/university/fcs_se/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.university.fcs_se.config;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket fcsApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.university.fcs_se.controller"))
+                .build();
+    }
+}
